07a8f8a4371f0db1f89a34c5d4ca170615c817c0
emiliano buscarol <emi_310397@hotmail.com.ar>
2021-08-26 13:56:32 -0300

    Added GetStudentGroup endpoint.

---
+++ presentation/Http/Presenters/StudentGroup/StudentGroupPresenter.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Presentation\Http\Presenters\StudentGroup;
+
+use Application\Results\StudentGroup\StudentGroupResult;
+use Domain\Entities\Student;
+use Infrastructure\Presenter\Contracts\PresenterInterface;
+
+class StudentGroupPresenter implements PresenterInterface
+{
+    private StudentGroupResult $result;
+
+    public function fromResult(StudentGroupResult $result): StudentGroupPresenter
+    {
+        $this->result = $result;
+        return $this;
+    }
+
+    public function toJson(): string
+    {
+        return json_encode($this->getData());
+    }
+
+    public function getData(): array
+    {
+        $studentGroup = $this->result->getStudentGroup();
+
+        $studentsIds = $this->getStudentsIds($studentGroup->getStudents()->toArray());
+
+        return [
+            'id' => $studentGroup->getId(),
+            'name' => $studentGroup->getName(),
+            'description' => $studentGroup->getDescription(),
+            'course' => $studentGroup->getCourse()->getId(),
+            'students' => $studentsIds
+        ];
+    }
+
+    private function getStudentsIds(array $students): array
+    {
+        $studentsIds = [];
+
+        /* @var Student $student */
+        foreach ($students as $student) {
+            $studentsIds[] = $student->getId();
+        }
+
+        return $studentsIds;
+    }
+}
