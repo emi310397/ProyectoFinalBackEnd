04178c62ab5d486896b886dae6fb2004f0eb9a6d
emiliano buscarol <emi_310397@hotmail.com.ar>
2021-10-04 19:30:38 -0300

    Translated adapter messages.

--- presentation/Http/Adapters/Teacher/CreateTeacherAdapter.php
+++ presentation/Http/Adapters/Teacher/CreateTeacherAdapter.php
@@ -28,15 +28,15 @@ class CreateTeacherAdapter extends CommandAdapter
     public function getMessages(): array
     {
         return [
-            self::FIRST_NAME_PARAM . 'required' => __('The id of the class is required'),
-            self::FIRST_NAME_PARAM . 'string' => __('The id of the class must be a number'),
-            self::LAST_NAME_PARAM . 'required' => __('The title of the task is required'),
-            self::LAST_NAME_PARAM . 'string' => __('The title of the task must be a string'),
-            self::EMAIL_PARAM . 'required' => __('The description of the task is required'),
-            self::EMAIL_PARAM . 'email' => __('The description of the task is required'),
-            self::EMAIL_PARAM . 'string' => __('The description of the task must be a string'),
-            self::PASSWORD_PARAM . 'required' => __('The start date of the task is required'),
-            self::PASSWORD_PARAM . 'string' => __('The start date of the task must be a valid date'),
+            self::FIRST_NAME_PARAM . 'required' => __('The user first name is required'),
+            self::FIRST_NAME_PARAM . 'string' => __('The user first name must be a string'),
+            self::LAST_NAME_PARAM . 'required' => __('The user last name is required'),
+            self::LAST_NAME_PARAM . 'string' => __('The user last name must be a string'),
+            self::EMAIL_PARAM . 'required' => __('The user email is required'),
+            self::EMAIL_PARAM . 'email' => __('The user email must be a string'),
+            self::EMAIL_PARAM . 'string' => __('The user email must is not valid'),
+            self::PASSWORD_PARAM . 'required' => __('The user password is required'),
+            self::PASSWORD_PARAM . 'string' => __('The user password must be a string'),
         ];
     }
 
