530f4ec9a1eba0163d368af474ac924deb87249a
emiliano <emi_310397@hotmail.com.ar>
2022-01-05 16:18:35 -0300

    Regenerated migrations.

--- database/migrations/Version20220105191641.php
+++ database/migrations/Version20220105191641.php
@@ -5,7 +5,7 @@ namespace Database\Migrations;
 use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema as Schema;
 
-class Version20211022203718 extends AbstractMigration
+class Version20220105191641 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -14,16 +14,18 @@ class Version20211022203718 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE activities (id INT AUTO_INCREMENT NOT NULL, task_id INT DEFAULT NULL, title LONGTEXT NOT NULL, type INT NOT NULL, description LONGTEXT NOT NULL, body LONGTEXT NOT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, INDEX IDX_B5F1AFE58DB60186 (task_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE assignments (id INT AUTO_INCREMENT NOT NULL, task_id INT DEFAULT NULL, student_id INT DEFAULT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, INDEX IDX_308A50DD8DB60186 (task_id), UNIQUE INDEX UNIQ_308A50DDCB944F1A (student_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE classes (id INT AUTO_INCREMENT NOT NULL, course_id INT DEFAULT NULL, title LONGTEXT NOT NULL, description LONGTEXT NOT NULL, from_date DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\', to_date DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\', deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, INDEX IDX_2ED7EC5591CC992 (course_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE classes (id INT AUTO_INCREMENT NOT NULL, course_id INT DEFAULT NULL, title LONGTEXT NOT NULL, description LONGTEXT NOT NULL, from_date DATE NOT NULL, to_date DATE NOT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, INDEX IDX_2ED7EC5591CC992 (course_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE courses (id INT AUTO_INCREMENT NOT NULL, teacher_id INT DEFAULT NULL, title LONGTEXT NOT NULL, description LONGTEXT NOT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, UNIQUE INDEX UNIQ_A9A55A4C41807E1D (teacher_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE sessions (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, hash LONGTEXT NOT NULL, expired TINYINT(1) NOT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, INDEX IDX_9A609D13A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE student_groups (id INT AUTO_INCREMENT NOT NULL, course_id INT DEFAULT NULL, name LONGTEXT NOT NULL, description LONGTEXT NOT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, INDEX IDX_7E5BE1F0591CC992 (course_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE student_student_group (student_group_id INT NOT NULL, student_id INT NOT NULL, INDEX IDX_B06BC5C64DDF95DC (student_group_id), INDEX IDX_B06BC5C6CB944F1A (student_id), PRIMARY KEY(student_group_id, student_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE tasks (id INT AUTO_INCREMENT NOT NULL, title LONGTEXT NOT NULL, description LONGTEXT NOT NULL, from_date DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\', to_date DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\', deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tasks (id INT AUTO_INCREMENT NOT NULL, title LONGTEXT NOT NULL, description LONGTEXT NOT NULL, from_date DATE NOT NULL, to_date DATE NOT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE p_class_task (task_id INT NOT NULL, p_class_id INT NOT NULL, INDEX IDX_B150EA658DB60186 (task_id), INDEX IDX_B150EA65370AB730 (p_class_id), PRIMARY KEY(task_id, p_class_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tokens (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, hash LONGTEXT NOT NULL, type INT NOT NULL, expired TINYINT(1) NOT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, INDEX IDX_AA5A118EA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email LONGTEXT NOT NULL, password LONGTEXT NOT NULL, status INT NOT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, rol VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE activities ADD CONSTRAINT FK_B5F1AFE58DB60186 FOREIGN KEY (task_id) REFERENCES tasks (id)');
         $this->addSql('ALTER TABLE assignments ADD CONSTRAINT FK_308A50DD8DB60186 FOREIGN KEY (task_id) REFERENCES tasks (id)');
         $this->addSql('ALTER TABLE assignments ADD CONSTRAINT FK_308A50DDCB944F1A FOREIGN KEY (student_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE classes ADD CONSTRAINT FK_2ED7EC5591CC992 FOREIGN KEY (course_id) REFERENCES courses (id)');
@@ -48,6 +50,7 @@ class Version20211022203718 extends AbstractMigration
         $this->addSql('ALTER TABLE classes DROP FOREIGN KEY FK_2ED7EC5591CC992');
         $this->addSql('ALTER TABLE student_groups DROP FOREIGN KEY FK_7E5BE1F0591CC992');
         $this->addSql('ALTER TABLE student_student_group DROP FOREIGN KEY FK_B06BC5C64DDF95DC');
+        $this->addSql('ALTER TABLE activities DROP FOREIGN KEY FK_B5F1AFE58DB60186');
         $this->addSql('ALTER TABLE assignments DROP FOREIGN KEY FK_308A50DD8DB60186');
         $this->addSql('ALTER TABLE p_class_task DROP FOREIGN KEY FK_B150EA658DB60186');
         $this->addSql('ALTER TABLE assignments DROP FOREIGN KEY FK_308A50DDCB944F1A');
@@ -55,6 +58,7 @@ class Version20211022203718 extends AbstractMigration
         $this->addSql('ALTER TABLE sessions DROP FOREIGN KEY FK_9A609D13A76ED395');
         $this->addSql('ALTER TABLE student_student_group DROP FOREIGN KEY FK_B06BC5C6CB944F1A');
         $this->addSql('ALTER TABLE tokens DROP FOREIGN KEY FK_AA5A118EA76ED395');
+        $this->addSql('DROP TABLE activities');
         $this->addSql('DROP TABLE assignments');
         $this->addSql('DROP TABLE classes');
         $this->addSql('DROP TABLE courses');
