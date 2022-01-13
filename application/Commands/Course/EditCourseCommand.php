9adbe41f4c8e6d9d1fcb6cf20b810f638347e0c2
emiliano <emi_310397@hotmail.com.ar>
2022-01-13 10:26:36 -0300

    Refactored EditCourse endpoint.

--- application/Commands/Course/EditCourseCommand.php
+++ application/Commands/Course/EditCourseCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Application\Commands\Course;
 
+use DateTime;
 use Domain\CommandBus\CommandInterface;
 use Domain\Entities\Course;
 
@@ -12,17 +13,23 @@ class EditCourseCommand implements CommandInterface
     private Course $course;
     private string $title;
     private string $description;
+    private DateTime $fromDate;
+    private DateTime $toDate;
     private array $days;
 
     public function __construct(
         Course $course,
         string $title,
         string $description,
+        string $fromDate,
+        string $toDate,
         array $days = null
     ) {
         $this->course = $course;
         $this->title = $title;
         $this->description = $description;
+        $this->fromDate = new DateTime($fromDate);
+        $this->toDate = new DateTime($toDate);
         $this->days = $days;
     }
 
@@ -39,6 +46,16 @@ class EditCourseCommand implements CommandInterface
     public function getDescription(): string
     {
         return $this->description;
+    }
+
+    public function getFromDate(): DateTime
+    {
+        return $this->fromDate;
+    }
+
+    public function getToDate(): DateTime
+    {
+        return $this->toDate;
     }
 
     public function getDays(): ?array
--- presentation/Http/Adapters/Course/EditCourseAdapter.php
+++ presentation/Http/Adapters/Course/EditCourseAdapter.php
@@ -19,6 +19,8 @@ class EditCourseAdapter extends CommandAdapter
     private const TITLE_PARAM = 'title';
     private const DESCRIPTION_PARAM = 'description';
     private const DAYS_PARAM = 'days';
+    private const FROM_DATE_PARAM = 'fromDate';
+    private const TO_DATE_PARAM = 'toDate';
 
     public function __construct(
         ValidatorServiceInterface $validator,
@@ -36,6 +38,8 @@ class EditCourseAdapter extends CommandAdapter
             self::DESCRIPTION_PARAM => 'bail|required|string',
             self::DAYS_PARAM => 'bail|array|nullable',
             self::DAYS_PARAM . '.*' => 'distinct|integer|in:' . implode(',', DaysOfTheWeek::ALL_DAYS),
+            self::FROM_DATE_PARAM => 'bail|required|date',
+            self::TO_DATE_PARAM => 'bail|required|date|after:' . self::FROM_DATE_PARAM,
         ];
     }
 
@@ -53,6 +57,11 @@ class EditCourseAdapter extends CommandAdapter
             self::DAYS_PARAM . '.*.distinct' => __('The days of the course can not be repeated'),
             self::DAYS_PARAM . '.*.integer' => __('The day of the course must be a number'),
             self::DAYS_PARAM . '.*.in' => __('The day of the course must be between 1 and 7'),
+            self::FROM_DATE_PARAM . 'required' => __('The start date of the task is required'),
+            self::FROM_DATE_PARAM . 'date' => __('The start date of the task must be a valid date'),
+            self::TO_DATE_PARAM . 'required' => __('The end date of the task is required'),
+            self::TO_DATE_PARAM . 'date' => __('The end date of the task must be a valid date'),
+            self::TO_DATE_PARAM . 'after' => __('The end date of the task must be higher than the start date'),
         ];
     }
 
@@ -69,7 +78,9 @@ class EditCourseAdapter extends CommandAdapter
             $course,
             $request->get(self::TITLE_PARAM),
             $request->get(self::DESCRIPTION_PARAM),
-            $request->get(self::DAYS_PARAM)
+            $request->get(self::DAYS_PARAM),
+            $request->get(self::FROM_DATE_PARAM),
+            $request->get(self::TO_DATE_PARAM)
         );
     }
 }
