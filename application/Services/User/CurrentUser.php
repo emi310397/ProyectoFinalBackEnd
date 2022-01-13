877b3496ce28d2d6a97f222344e9d692a62e7328
emiliano <emi_310397@hotmail.com.ar>
2022-01-13 09:32:15 -0300

    Improved CurrentUser service.

--- application/Services/User/CurrentUser.php
+++ application/Services/User/CurrentUser.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Application\Services\User;
 
+use Domain\Entities\Student;
+use Domain\Entities\Teacher;
 use Domain\Entities\User;
 use Domain\Interfaces\CurrentUserInterface;
 
@@ -14,6 +16,22 @@ final class CurrentUser implements CurrentUserInterface
     public function getUser(): ?User
     {
         return $this->user;
+    }
+
+    public function getTeacher(): ?Teacher
+    {
+        $user = $this->user;
+
+        /* @var Teacher $user */
+        return $user;
+    }
+
+    public function getStudent(): ?Student
+    {
+        $user = $this->user;
+
+        /* @var Student $user */
+        return $user;
     }
 
     public function setUser(User $user): void
--- domain/Interfaces/CurrentUserInterface.php
+++ domain/Interfaces/CurrentUserInterface.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Domain\Interfaces;
 
+use Domain\Entities\Student;
+use Domain\Entities\Teacher;
 use Domain\Entities\User;
 
 interface CurrentUserInterface
@@ -11,4 +13,8 @@ interface CurrentUserInterface
     public function setUser(User $user): void;
 
     public function getUser(): ?User;
+
+    public function getTeacher(): ?Teacher;
+
+    public function getStudent(): ?Student;
 }
